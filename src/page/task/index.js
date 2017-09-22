8d8a617a78340eed6d0c55a0924882a1b458def6
韩梦晴 <657691495@qq.com>
2017-09-22 19:56:04 +0800

    新增列表公共样式、删除多余样式、实现tab切换

--- src/page/task/index.js
+++ src/page/task/index.js
@@ -4,7 +4,7 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        tab_status: 0
     },
 
     /**
@@ -12,6 +12,7 @@ Page({
      */
     onLoad: function (options) {
 
+        //模拟登录跳转
         // wx.navigateTo({
         //     url: '/page/login/index',
         //     success: function (res) { },
@@ -20,8 +21,26 @@ Page({
         // })
 
     },
-    jumpDetail:function(){
-            url:'';
+    // 切换类型
+    switch_tab: function (e) {
+        this.setData({
+            tab_status: e.target.id
+        })
+    },
+    // 折叠面板
+    change_status: function (e) {
+        //来不及写了
+        console.log(e)
+
+    },
+    // 点击列表跳转到详情
+    jumpDetail: function (e) {
+        wx.navigateTo({
+            url: '',
+            success: function (res) { },
+            fail: function (res) { },
+            complete: function (res) { },
+        })
     }
 
 })
