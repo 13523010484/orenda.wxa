a864841876daaa8e14a754a96fd63f9b1a8971d0
韩梦晴 <657691495@qq.com>
2017-09-23 20:23:01 +0800

    修改bug列表、修改我的任务列表

--- src/page/me/task.js
+++ src/page/me/task.js
@@ -1,3 +1,4 @@
+var app = getApp()
 Page({
 
     /**
@@ -12,10 +13,39 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
-
+        this.getUserInfo()
     },
-
+    /**
+     * 获取微信公共信息
+    */
+    getUserInfo() {
+        var that = this
+        if (app.globalData.haswxLogin === false) {
+            wx.login({
+                success: _getUserInfo
+            })
+        } else {
+            _getUserInfo()
+        }
+        function _getUserInfo() {
+            wx.getUserInfo({
+                success(res) {
+                    app.globalData.haswxLogin = res.userInfo
+                    that.setData({
+                        userInfo: res.userInfo
+                    })
+                },
+                //用户拒绝微信授权的情况，显示一个默认头像
+                fail(res) {
+                    that.setData({
+                        userInfo: {
+                            avatarUrl: '/image/task_user.png'
+                        }
+                    })
+                }
+            })
+        }
+    },
     // 折叠面板
     change_status: function (e) {
         this.data[e.currentTarget.id] = !this.data[e.currentTarget.id]
