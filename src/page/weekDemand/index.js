8cf59bf34b833ab70e52f3b54aba9048914dff3b
jiaqiuhua <flower346@qq.com>
2017-09-25 15:33:22 +0800

    需求列表页修改

--- src/page/weekDemand/index.js
+++ src/page/weekDemand/index.js
@@ -1,66 +1,32 @@
-// page/weekDemand/index.js
+var app = getApp()
 Page({
 
         /**
          * 页面的初始数据
          */
         data: {
-
+                open1: true,
+                open2: false
         },
 
         /**
          * 生命周期函数--监听页面加载
          */
-        onLoad: function (options) {
-
+        onLoad: function () {
         },
-
-        /**
-         * 生命周期函数--监听页面初次渲染完成
-         */
-        onReady: function () {
-
+        // 折叠面板
+        change_status: function (e) {
+                this.data[e.currentTarget.id] = !this.data[e.currentTarget.id]
+                this.setData(this.data)
         },
-
-        /**
-         * 生命周期函数--监听页面显示
-         */
-        onShow: function () {
-
-        },
-
-        /**
-         * 生命周期函数--监听页面隐藏
-         */
-        onHide: function () {
-
-        },
-
-        /**
-         * 生命周期函数--监听页面卸载
-         */
-        onUnload: function () {
-
-        },
-
-        /**
-         * 页面相关事件处理函数--监听用户下拉动作
-         */
-        onPullDownRefresh: function () {
-
-        },
-
-        /**
-         * 页面上拉触底事件的处理函数
-         */
-        onReachBottom: function () {
-
-        },
-
-        /**
-         * 用户点击右上角分享
-         */
-        onShareAppMessage: function () {
-
+        // 点击列表跳转到详情
+        jumpDetail: function (e) {
+                // e.currentTarget.id 仅用来模拟，为了动态显示任务详情页情况
+                wx.navigateTo({
+                        url: '/page/taskDetail/index?id=' + e.currentTarget.id,
+                        success: function (res) { },
+                        fail: function (res) { },
+                        complete: function (res) { },
+                })
         }
 })
