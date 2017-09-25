6fe9818461154f27235237bca3b2f29cfc98e2e0
韩梦晴 <657691495@qq.com>
2017-09-25 12:27:02 +0800

    新增追踪模块

---
+++ src/page/manage/index.js
@@ -0,0 +1,75 @@
+// page/manage/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        tab_status: 0
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    // 切换类型
+    switch_tab: function (e) {
+        this.setData({
+            tab_status: e.target.id
+        })
+    },
+    // 点击列表跳转到详情
+    jumpDetail: function (e) {
+        // e.currentTarget.id 仅用来模拟，为了动态显示任务详情页情况
+        wx.navigateTo({
+            url: '/page/taskDetail/index?id=' + e.currentTarget.id,
+            success: function (res) { },
+            fail: function (res) { },
+            complete: function (res) { },
+        })
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
