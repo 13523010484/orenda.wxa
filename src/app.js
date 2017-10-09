4dc914fd8bb27ff7fa6f57273435d69370f71d2e
jiaqiuhua <flower346@qq.com>
2017-10-09 15:10:19 +0800

    新增登录页面接口

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const config = require('./config');
-function request(url, param, callback) {
+function request(url, param, callback, method) {
         wx.request({
                 url: url,
                 data: param,
-                method: 'POST',
+                method: method || 'GET',
                 dataType: 'json',
                 header: { 'content-type': 'application/json' },
-                success: function (res) {
+                success: function (ret) {
+                        var data = ret.data.hasErrors == true ? { code: 0, msg: ret.data.message } : { code: 1, data: ret.data.data };
+                        return typeof callback == "function" && callback
                 },
                 fail: function () {
-
+                        var data = { code: 401, msg: '网络连接错误！' };
+                        return typeof callback == "function" && callback
                 }
         })
 }
@@ -18,7 +21,7 @@ App({
         request: request,
         api: config,
         onLaunch: function () {
-                console.log('App Launch')
+                console.log('onLaunch')
         },
         onShow: function () {
                 console.log('App Show')
@@ -27,7 +30,8 @@ App({
                 console.log('App Hide')
         },
         globalData: {
-                haswxLogin: false,
-                openid: null
+                // haswxLogin: false,
+                // openid: null
+                haswxLogin: wx.getStorageSync('loginInfo') || false
         }
 })
--- src/page/login/index.js
+++ src/page/login/index.js
@@ -1,24 +1,63 @@
+//获取app.js
+var app = getApp()
+const loginUrl = require('../../config').loginUrl
+function get_data(that) {
+        var $this = that
+        app.request(loginUrl, { login_name: $this.data.login_name, login_password: $this.data.login_password }, function (res) {
+                if (res.code == 1) {
+                        wx.setStorageSync('loginInfo', res.data);
+                        app.globalData.hasLogin == res.data;
+                        wx.switchTab({
+                                url: './page/taskDetail/index',
+                        })
+                } else {
+                        wx.showModal({
+                                title: '提示',
+                                content: res.msg,
+                                showCancel: false,
+                                confirmColor: '#55b2ff',
+                                success: function (res) {
+                                        if (res.confirm) {
+                                                console.log('点击弹出框确认按钮后，再次进行操作')
+                                        }
+                                }
+                        })
+                }
+        }, 'POST')
+}
 Page({
+        /* 页面初始化数据 */
+        data: {
+                login_name: '',
+                login_password: ''
+        },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-
-    },
-    go_login: function () {
-        //此处默认登录成功，模拟流程，实际情况则需要另外的操作
-        wx.navigateBack({
-            delta: 1
-        })
-    }
-
+        /* 页面登录*/
+        // onLaunch: function () {
+        //         wx.login({
+        //                 success: function (res) {
+        //                         if (res.code) {
+        //                                 wx.request({
+        //                                         url: '',
+        //                                         data: res.code
+        //                                 })
+        //                         } else {
+        //                                 console.log('用户登录失败！' + res.errMsg)
+        //                         }
+        //                 }
+        //         })
+        // },
+        bindKeyInput: function (e) {
+                this.setData({
+                        login_name: e.detail.value
+                })
+        },
+        bindPassword: function (e) {
+                this.setData({
+                        login_password: e.detail.value
+                })
+        },
+        go_login: function () {
+                get_data(this);
+        }
 })
